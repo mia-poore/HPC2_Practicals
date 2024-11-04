499f2a40f9a4767fe53ccc52b92ef4595a3d50a4
Charlotte James <cj9491@bc4login2.bc4.acrc.priv>
2024-11-04 21:24:39 +0000

    update slurm script

--- run_analysis.sh
+++ run_analysis.sh
@@ -18,7 +18,14 @@ echo 'Starting analysis'
 
 cd ~/ahds_practical/conda-hpc-snakemake-example/code/
 
-source run_all.sh
+Rscript 1_get_unique_conditions.R
 
+{       read
+        while IFS=, read -r line;
+        do
+                Rscript 2_find_side_effects.R "${line}"
+                Rscript 3_plot_wordcloud.R "${line}"
+        done
 
+}< ../data/derived/top_conditions.csv
 
